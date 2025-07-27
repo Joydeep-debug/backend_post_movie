c237f4895025cb2a41766726d84272fd64b27c1c
Joydeep-debug <joydeepchkrbrt99@gmail.com>
2025-07-28 00:23:12 +0530

    feat: Adding api to post movie

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const {initializeDatabase} = require("./db/db.connect");
+const Movie = require("./models/movie.models");
+const app = express();
+app.use(express.json());
+initializeDatabase();
+
+async function createMovie(newMovie){
+    try{
+        const movie = new Movie(newMovie);
+        const saveMovie = await movie.save();
+        return saveMovie;
+    } catch(error){
+        throw error
+    }
+}
+
+app.post("/movies", async (req, res) => {
+    try{
+        const savedMovie = await createMovie(req.body);
+        res.status(201).json({message: "Movie added successfully.", movie: savedMovie});
+    } catch(error){
+        res.status(500).json({error: "Failed to add movie."});
+    }
+})
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+});
